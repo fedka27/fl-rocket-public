caabb09fcbd71bb5814f2df90c692960ef38f1be
Fedya Trubitsyn <trubitsyn.fedya@yandex.ru>
2018-06-15 13:32:15 +0200

    [!] Password for keystore fixed;

--- app/src/main/java/wash/rocket/xor/rocketwash/ui/WashServiceInfoBaseFragment.java
+++ app/src/main/java/wash/rocket/xor/rocketwash/ui/WashServiceInfoBaseFragment.java
@@ -103,7 +103,12 @@ public abstract class WashServiceInfoBaseFragment extends BaseFragment {
                              double amount,
                              String title,
                              String description) {
-        PayFormActivity.init("1509384921522DEMO", "eo8bv0zyqde8c6cn", "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv5yse9ka3ZQE0feuGtemYv3IqOlLck8zHUM7lTr0za6lXTszRSXfUO7jMb+L5C7e2QNFs+7sIX2OQJ6a+HG8kr+jwJ4tS3cVsWtd9NXpsU40PE4MeNr5RqiNXjcDxA+L4OsEm/BlyFOEOh2epGyYUd5/iO3OiQFRNicomT2saQYAeqIwuELPs1XpLk9HLx5qPbm8fRrQhjeUD5TLO8b+4yCnObe8vy/BMUwBfq+ieWADIjwWCMp2KTpMGLz48qnaD9kdrYJ0iyHqzb2mkDhdIzkim24A3lWoYitJCBrrB2xM05sm9+OdCI1f7nPNJbl5URHobSwR94IRGT7CJcUjvwIDAQAB")
+        PayFormActivity.init
+                (
+                        "1509384921522DEMO",
+                        "eo8bv0zyqde8c6cn",
+                        "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv5yse9ka3ZQE0feuGtemYv3IqOlLck8zHUM7lTr0za6lXTszRSXfUO7jMb+L5C7e2QNFs+7sIX2OQJ6a+HG8kr+jwJ4tS3cVsWtd9NXpsU40PE4MeNr5RqiNXjcDxA+L4OsEm/BlyFOEOh2epGyYUd5/iO3OiQFRNicomT2saQYAeqIwuELPs1XpLk9HLx5qPbm8fRrQhjeUD5TLO8b+4yCnObe8vy/BMUwBfq+ieWADIjwWCMp2KTpMGLz48qnaD9kdrYJ0iyHqzb2mkDhdIzkim24A3lWoYitJCBrrB2xM05sm9+OdCI1f7nPNJbl5URHobSwR94IRGT7CJcUjvwIDAQAB"
+                )
                 .prepare(
                         String.valueOf(orderId),
                         Money.ofRubles(amount),
